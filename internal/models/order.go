2fbd6c58db1dca5050b83998dddd21461105ced5
Vic428-human <z0983195379a@gmail.com>
2026-01-01 23:04:04 +0800

    create module for golang

---
+++ internal/models/order.go
@@ -0,0 +1,23 @@
+package models
+
+var (
+	OrderStatues = []string{
+		"Order placed",
+		"Preparing",
+		"Baking",
+		"Quality Check",
+		"Ready",
+	}
+
+	PizzaTypes = []string{
+		"Cheese",
+		"Pepperoni",
+		"Veggie",
+	}
+
+	PizzaSizes = []string{
+		"Small",
+		"Medium",
+		"Large",
+	}
+)
