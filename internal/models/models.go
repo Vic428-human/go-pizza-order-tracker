dc9398788055ea646a2a8db3f81eef5c3f2ecac5
Vic428-human <z0983195379a@gmail.com>
2026-01-03 11:52:57 +0800

    將order model 跟 db連線等另外封裝在 初始化db裡，用意為之後使用 dbModel  可以調用其相關方法。

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,32 +1,36 @@
 package models
 
 import (
+	"fmt"
+
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
 type DBModel struct {
+	Order OrderModel
 	DB *gorm.DB
 }
 
 func InitDB(dataSourceName string)(*DBModel, error){
 	
-	// 創建數據庫連接
-	// https://zhuanlan.zhihu.com/p/651250516
-	// 第一個參數 Dialector，用來指定數據庫的類型，像是 mysql / sqlite / postgres 等等
-	// db 是由 gorm.Open 回傳的 *gorm.DB 物件
+	// 第一個參數 Dialector，用來指定數據庫的類型，像是 mysql / sqlite / postgres 等，db 是由 gorm.Open 回傳的 *gorm.DB 物件， https://zhuanlan.zhihu.com/p/651250516
 	db, err := gorm.Open(sqlite.Open(dataSourceName), &gorm.Config{}) // & 表示取址，支持就地修改，如設置日誌或連接池
 	
-	if err != nil {
-		return nil, err
+	if err != nil { // 若錯誤發生
+		return nil, fmt.Errorf("資料庫連接失敗(ex: 檔案路徑錯誤、目錄不存在或無寫入權限): %w", err)
 	}
 
-	// 將 gorm 的實例設定到 DBModel，讓該結構體能直接暴露資料庫的方法 => dbModel.DB.Create(&user)
+	err = db.AutoMigrate(&Order{}, &OrderItem{})
+	if err != nil {
+		return nil, fmt.Errorf("已經成功連接，但執行 schema 遷移時出錯(ex:1. 模型 tag 不支援 SQLite語法 2.更改欄位類型、刪除欄位、check constraint 等，SQLite 限制多，GORM 會重建表格失敗。 3.複合唯一索引、view 依賴、foreign key 順序問題等)")
+	}
+	
 	dbModel := &DBModel{ 
 		DB: db,
+		Order: OrderModel{DB: db},
 	}
-
-
 	return dbModel, nil
+
 }
 
