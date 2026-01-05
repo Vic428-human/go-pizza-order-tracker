2b723aa2379ba0a9dc48d2badfc1d37d9f069d50
Vic428-human <z0983195379a@gmail.com>
2026-01-05 20:34:25 +0800

    定義對template傳入變數值有關的func，跟pizza 大小種類有關的

--- cmd/customer.go
+++ cmd/customer.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"net/http"
+	"pizza-tracker-go/internal/models"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -9,10 +12,7 @@ type OrderFormData struct {
 	PizzaSizes []string
 }
 
-// dive 是 go-playground/validator 提供的特殊標籤。
-// 它用於啟用對 slice/array/map 內部元素的遞歸驗證。
-// 若結構體中包含嵌套的切片或數組，且需要驗證其內部字段，必須加上 dive，否則只會驗證外層容器本身（如長度），不會驗證內部元素的字段。
-
+// dive 是 go-playground/validator 提供的特殊標籤，它用於啟用對 slice/array/map 內部元素的遞歸驗證，若結構體中包含嵌套的切片或數組，且需要驗證其內部字段，必須加上 dive，否則只會驗證外層容器本身（如長度），不會驗證內部元素的字段。
 type OrderRequest struct {
 	Name         string   `json:"name" binding:"required,min=2,max=100"`
 	Phone        string   `json:"phone" binding:"required, min=10,max=20"`
@@ -23,5 +23,8 @@ type OrderRequest struct {
 }
 
 func (h *Handler) ServiceNewOrderList(c *gin.Context) {
-	c.HTML()
+	c.HTML(http.StatusOK, "order.tmpl", OrderFormData{
+		PizzaTypes: models.PizzaTypes,
+		PizzaSizes: models.PizzaSizes,
+	})
 }
