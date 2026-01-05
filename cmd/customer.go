e1b609b1e2d6657c9e352d936edadd91d078f851
Vic428-human <z0983195379a@gmail.com>
2026-01-05 13:18:48 +0800

    定義顧客發送訂單請求時的結構體

---
+++ cmd/customer.go
@@ -0,0 +1,19 @@
+package main
+
+type OrderFormData struct {
+	PizzaTypes []string
+	PizzaSizes []string
+}
+
+// dive 是 go-playground/validator 提供的特殊標籤。
+// 它用於啟用對 slice/array/map 內部元素的遞歸驗證。
+// 若結構體中包含嵌套的切片或數組，且需要驗證其內部字段，必須加上 dive，否則只會驗證外層容器本身（如長度），不會驗證內部元素的字段。
+
+type OrderRequest struct {
+	Name         string   `json:"name" binding:"required,min=2,max=100"`
+	Phone        string   `json:"phone" binding:"required, min=10,max=20"`
+	Address      string   `json:"address" binding:"required,min=5,max=200"`
+	Size         string   `json:"size" binding:"required,dive,valid_pizza_size"`
+	PizzaTypes   []string `json:"pizzaTypes" binding:"required,dive,valid_pizza_type"`
+	Instructions string   `json:"instructions" binding:"max=500"`
+}
