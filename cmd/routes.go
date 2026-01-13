1943f1f56146d8900e45ddf76e18e0fcaee2da01
Vic428-human <z0983195379a@gmail.com>
2026-01-14 06:38:20 +0800

    切版:tmpl 的表單畫面

--- cmd/routes.go
+++ cmd/routes.go
@@ -12,5 +12,4 @@ func setupRoutes(router *gin.Engine, h *Handler) {
 	router.GET("/customer/:id", h.serveCustomer) // 查看顧客訂單內容
 	// 基本实现：使用 router.Static 方法来设置静态文件目录，例如 router.Static("/static", "./public")，这将把 public 目录下的所有文件映射到 /static 路径。
 	router.Static("/static", "./templates/static")
-
 }
