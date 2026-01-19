9ba82ce8be54e762dc5c0036d174a4881a5df07a
Vic428-human <z0983195379a@gmail.com>
2026-01-19 12:13:50 +0800

    使用 go的bcrypt，1.對密碼加鹽 2.確認密碼已經轉成

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/glebarez/sqlite v1.11.0
 	github.com/go-playground/validator/v10 v10.30.1
 	github.com/teris-io/shortid v0.0.0-20220617161101-71ec9f2aa569
+	golang.org/x/crypto v0.46.0
 	gorm.io/gorm v1.31.1
 )
 
@@ -40,7 +41,6 @@ require (
 	github.com/ugorji/go/codec v1.3.1 // indirect
 	go.uber.org/mock v0.5.0 // indirect
 	golang.org/x/arch v0.23.0 // indirect
-	golang.org/x/crypto v0.46.0 // indirect
 	golang.org/x/mod v0.30.0 // indirect
 	golang.org/x/net v0.47.0 // indirect
 	golang.org/x/sync v0.19.0 // indirect
--- internal/models/user.go
+++ internal/models/user.go
@@ -1,6 +1,11 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"errors"
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+)
 
 type User struct { // 定義一個 User struct，對應到資料庫中的 users table。
 	ID       string `gorm:"primaryKey"`
@@ -10,6 +15,20 @@ type User struct { // 定義一個 User struct，對應到資料庫中的 users 
 
 type UserModel struct {
 	DB *gorm.DB // 持有 *gorm.DB，用來執行資料庫操作。
+}
+
+func HashPassword(password string) (string, error) {
+	// 以给定的Cost返回密码的bcrypt哈希。如果给定的成本小于MinCost，则将成本设置为DefaultCost（10）
+    bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+    return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	// 用于比对bcrypt哈希字符串和提供的密码明文文本是否匹配
+    if err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password)); err != nil{
+		return nil, errors.New("Invalid Credientials")
+	}
+	return err == nil
 }
 
 // 這是一個方法，綁定在 UserModel 上。
@@ -28,9 +47,12 @@ func (u *UserModel) AuthenticateUser(username, password string) (*User, error) {
 	}
 	
 
-	// 密碼不應該存明文，通常會用 bcrypt 或其他 hash 函式加密。
-
+	// 密碼不應該存明文，通常會用 bcrypt 或其他 hash 函式加密 => https://kryiea.github.io/back-end/go/framework/encipherframework/bcrypt%E5%8A%A0%E5%AF%86.html#_4-1-bcrypt%E5%8C%85%E4%BB%8B%E7%BB%8D
+	hash, _ := HashPassword(password) 
+	fmt.Fprintln(w, "Hash:    ", hash) // Hash:     $2a$14$Ael8nW7UF/En/iI7LGdyBuaIO8VREbL2CAShRN0EUQHqtmOHXh.XK
+	match := CheckPasswordHash(password, hash) 
+	fmt.Fprintln(w, "Match:   ", match) // Match:    true
 	
-	return &User,nil // 成功找到的話，就回傳 *User。
+	return &User, nil // 成功找到的話，就回傳 *User。
 
 }
