598c4ea7812f7cacb4b3436ebf1bd1482cc53739
Vic428-human <z0983195379a@gmail.com>
2026-01-20 05:44:46 +0800

    定義User model在初始化DB裡 2.修改建立資料庫連線跟更新資料表的錯誤訊息

--- internal/models/models.go
+++ internal/models/models.go
@@ -35,7 +35,7 @@ type DBModel struct {
 	// 分別是 Order 跟 DB 欄位
 	DB    *gorm.DB
 	Order OrderModel // *gorm.DB
-
+	User  UserModel
 }
 
 // 接收一個 *DBModel 型別指標
@@ -45,18 +45,19 @@ func InitDB(dataSourceName string) (*DBModel, error) {
 	// 第一個參數 Dialector，用來指定數據庫的類型，像是 mysql / sqlite / postgres 等，db 是由 gorm.Open 回傳的 *gorm.DB 物件， https://zhuanlan.zhihu.com/p/651250516
 	db, err := gorm.Open(sqlite.Open(dataSourceName), &gorm.Config{}) // & 表示取址，支持就地修改，如設置日誌或連接池
 
-	if err != nil { // 若錯誤發生
-		return nil, fmt.Errorf("資料庫連接失敗(ex: 檔案路徑錯誤、目錄不存在或無寫入權限): %w", err)
+	if err != nil {
+		return nil, fmt.Errorf("建立資料庫連線(資料庫不存在、連線字串錯誤、驅動問題): %v", err)
 	}
 
-	err = db.AutoMigrate(&Order{}, &OrderItem{})
+	err = db.AutoMigrate(&Order{}, &OrderItem{}, &User{})
 	if err != nil {
-		return nil, fmt.Errorf("已經成功連接，但執行 schema 遷移時出錯(ex:1. 模型 tag 不支援 SQLite語法 2.更改欄位類型、刪除欄位、check constraint 等，SQLite 限制多，GORM 會重建表格失敗。 3.複合唯一索引、view 依賴、foreign key 順序問題等)")
+		return nil, fmt.Errorf("建立/更新資料表(權限不足、SQL 執行錯誤、模型定義不合法): %v", err)
 	}
 
 	dbModel := &DBModel{
-		DB:    db,                 // *gorm.DB 把sqlite的 db gorm物件覆寫
-		Order: OrderModel{DB: db}, // 把sqlite的 db gorm物件覆寫在 OrderModel裡的 DB
+		DB:    db,
+		Order: OrderModel{DB: db}, // 複寫 pass db connection 給結構體
+		User:  UserModel{DB: db},
 	}
 	return dbModel, nil
 
