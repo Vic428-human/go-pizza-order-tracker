c659f900bf0c484e7907c13b854f91b34610cfb2
Vic428-human <z0983195379a@gmail.com>
2026-01-20 06:00:53 +0800

    feat: Handler綁定 User模組

--- cmd/handler.go
+++ cmd/handler.go
@@ -7,11 +7,14 @@ import "pizza-tracker-go/internal/models"
 // 用來管理應用的主要邏輯層跟服務層
 // orders 指向 OrderModel 指標，用來處理有關訂單的資料操作，像是建立/查詢
 type Handler struct {
-	// 可以透過 import 引用其他 models 
+	// 可以透過 import 引用其他 models
+	// 這裡的 orders/users就是資料表的名稱，像是INSERT INTO的時候抓的就是這裡的欄位
 	orders *models.OrderModel
-	// 也可以直接調用 cmd 目錄下的其他功能模組
-}
+	users  *models.UserModel
 
+	// 也可以直接調用 cmd 目錄下的其他功能模組
+	// ...
+}
 
 // 1. 單元測試時，可以注入假的 MockOrderModel，輕鬆模擬資料庫行為
 // 2. 未來換成 Redis 或其他資料庫，只需要修改 NewHandler 的呼叫處理即可
@@ -24,6 +27,3 @@ func NewHandler(dbModel *models.DBModel) *Handler {
 	return &Handler{orders: &dbModel.Order} // &dbModel.Order 則是 指向這個值的指標 (OrderModel)*。
 	// 這樣 Handler 就能透過 orders 操作 OrderModel 的方法。
 }
-
-
-
