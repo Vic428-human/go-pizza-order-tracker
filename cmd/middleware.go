5b9543edaed226094c99600c69e87b81b2887ec8
Vic428-human <z0983195379a@gmail.com>
2026-01-21 10:35:31 +0800

    README: gin-contrib 說明

--- cmd/middleware.go
+++ cmd/middleware.go
@@ -2,6 +2,7 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+// https://zhuanlan.zhihu.com/p/30184285330
 func (h *Handler) AuthMiddleware() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		// get session
